d9946c9763548cba7f80b52663b20f1db5aad1e7
mohamed-zkaria-sultan <mohamed.zkaria.sultan@gmail.com>
2025-01-15 18:00:04 +0200

    pro

---
+++ app.py
@@ -0,0 +1,12 @@
+import helper
+import streamlit as st 
+import pickle
+
+model = pickle.load(open("Art/svc.pkl",'rb'))
+
+text = st.text_input('enter your review')
+
+text = helper.text_preprocessing(text)
+
+if st.button("predict"):
+    model.predict(text)
---
+++ helper.py
@@ -0,0 +1,31 @@
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import nltk
+import re
+import pickle
+
+tf = pickle.load(open("Art/tf.pkl",'rb'))
+
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+
+stop_words = stopwords.words('english')
+stemmer = PorterStemmer()
+
+def text_preprocessing(text):
+  ## lower case
+  text = text.lower()
+  ## special charcter
+  text = re.sub('[^a-zA-z]', ' ', text)
+  ## Tokinzation
+  text = word_tokenize(text)
+  ## stopwords
+  text = [word for word in text if word not in stop_words]
+  ## lemmetization
+  text = [stemmer.stem(word) for word in text]
+  text = ' '.join(text)
+  text = tf.transform([text])
+  return text
