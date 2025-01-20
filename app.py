0bd9d14f3e631280b71956fe3e13076fbe815544
mohamed-zkaria-sultan <mohamed.zkaria.sultan@gmail.com>
2025-01-20 19:56:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import helper
 import streamlit as st 
 import pickle
 
-model = pickle.load(open("artifacts/dt.pkl",'rb'))
+model = pickle.load(open("artifacts/lr.pkl",'rb'))
 
 text = st.text_input('enter your review')
 
